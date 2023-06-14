9a4e7b49d9b07df5b70334bcc206be9783235e84
IAMAFIVESTARMAN <99925216+IAMAFIVESTARMAN@users.noreply.github.com>
2023-06-14 15:05:54 +0530

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,10 +3,8 @@ export const Modal = (props) => {
     <>
       <div className="z-20 absolute  left-64 translate-x-32 bg-slate-50 py-20 w-3/4">
         <p className="mb-24 text-8xl">Rules of the Game</p>
-        With a plus button and a minus button to correct both player scores.
-        After 1 player got 21 points it should show that that player is the
-        winner. The winning player always has to win with 2 points advantage.
-        When its 20-20 the winning point will be 22, etc.
+        A plus button will update the socre and  a minus button to reduce player scores. then it's player 2's turn. After a player gets 11 points it should show that that player is the winner. 
+The winning player always has to win with 2 points advantage. When its 10-10 the winning point will be 12
         <button
           onClick={props.onClose}
           className="absolute -top-1/4 -right-10 py-5 px-5 bg-slate-950 text-yellow-50 "
